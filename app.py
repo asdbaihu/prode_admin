dee254be331f077cd9d39e19db7fdb84958b2488
jnrdrgz <yosoyjjjuan@gmail.com>
2019-08-18 19:41:34 -0300

    Almost entirely rewritten

--- app.py
+++ app.py
@@ -1,172 +1,384 @@
-from flask import Flask, render_template, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
-
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 from models import *
 
+class PartidoSchema(ma.Schema):
+	class Meta:
+		fields = ['id', 'fecha_id', 'local', 'visitante', 'resultado', 'punto_personalizado', 'pleno_personalizado']
+
+class FechaSchema(ma.Schema):
+	partidos = ma.Nested(PartidoSchema, many=True)
+	class Meta:
+		fields = ['id', 'prode_id', 'numero', 'partidos']
+
+class ProdeSchema(ma.Schema):
+	fechas = ma.Nested(FechaSchema, many=True)
+	class Meta:
+		fields = ['id', 'nombre', 'fechas']
+
+class PronosticoSchema(ma.Schema):
+	class Meta:
+		fields = ['participante_id', 'partido_id', 'resultado']
+
+class ParticipanteSchema(ma.Schema):
+	pronosticos = ma.Nested(PronosticoSchema, many=True)
+	class Meta:
+		fields = ['id', 'nombre', 'puntos', 'plenos', 'pronosticos']
+
+
+prode_schema = ProdeSchema(strict=True)
+prodes_schema = ProdeSchema(many=True, strict=True)
+
+partido_schema = PartidoSchema(strict=True)
+partidos_schema = PartidoSchema(many=True, strict=True)
+
+fecha_schema = FechaSchema(strict=True)
+fechas_schema = FechaSchema(many=True, strict=True)
+
+pronostico_schema = PronosticoSchema(strict=True)
+pronosticos_schema = PronosticoSchema(many=True, strict=True)
+
+participante_schema = ParticipanteSchema(strict=True)
+participantes_schema = ParticipanteSchema(many=True, strict=True)
+
 @app.route('/')
-def home():
-    return '<a href="/prodes">prodes<a><br><a href="/crear_prode">crear prode<a>'
+def hello():
+	return "Hi"
 
-@app.route('/crear_prode', methods=['GET', 'POST'])
-def crear_prode():
-	errors = []
-	if request.method == "POST": 
-		try:
-			nombre = request.form['nombre']
-			#print(nombre)
-			#pr = request.get(nombre)
-			#participante1 = request.form['participante1']
-			cantidad_participantes = request.form['cantidad']
+#----------------------#
 
-			participantes = []
-			for i in range(int(cantidad_participantes)):
-				participantes.append(request.form['participante' + str(i+1)])
-			
-		except Exception as e:
-			print(e)
-			errors.append("unable to ")
-			return render_template('agregar_prode.html', errors=errors)
+#Prode methods
 
+#----------------------#
 
-		if nombre:
-			try:
-				#print(nombre)
-				p = Prode(nombre)
-				db.session.add(p)
-				print(p.id)
-				db.session.commit()
-				#part = Participante(participante1, db.session.filter_by(nombre=nombre).all().id)
-				
-				new_id = db.session.query(Prode).order_by(Prode.id.desc()).first().id 
-				#print(participante1)
-				
-				#part = Participante(participante1, new_id)
-				
-				parts_objs = [Participante(p, new_id) for p in participantes]
+@app.route('/api/prode', methods=['POST'])
+def agregar_prode():
+	if request.method == "POST":
+		nombre = request.json['nombre']
+		print(nombre)
 
-				db.session.add_all(parts_objs)
-				db.session.commit()
-			except Exception as e:
-				print(e)
-				errors.append("Unable to add to database")
+		nuevo_prode = Prode(nombre)
 
+		db.session.add(nuevo_prode)
+		db.session.commit()
+
+		return prode_schema.jsonify(nuevo_prode)
+
+@app.route('/api/prode', methods=['GET'])
+def get_prode():
+	if request.method == "GET":
+		todos = Prode.query.all()
+		r = prodes_schema.dump(todos)
+
+		return jsonify(r.data)
+
+@app.route('/api/prode/<id>', methods=['GET'])
+def get_prode_by_id(id):
+	if request.method == "GET":
+		prode = Prode.query.get(id)
+		return prode_schema.jsonify(prode)
+
+@app.route('/api/prode/<id>', methods=['PUT'])
+def actualizar_prode(id):
+	if request.method == "PUT":
+		prode = Prode.query.get(id)
 		
-	return render_template('agregar_prode.html', errors=errors)
+		nombre = request.json['nombre']
+
+		prode.nombre = nombre
+		
+		db.session.commit()
+
+		return prode_schema.jsonify(prode)
+
+@app.route('/api/prode/<id>', methods=['DELETE'])
+def eliminar_prode(id):
+	if request.method == "DELETE":
+		prode = Prode.query.get(id)
+
+		db.session.delete(prode)
+		db.session.commit()
+
+		return prode_schema.jsonify(prode)
+
+#----------------------#
+
+#Participante methods
+
+#----------------------#
+
+@app.route('/api/participante', methods=['POST'])
+def agregar_participante():
+	if request.method == "POST":
+		nombre = request.json['nombre']
+		prode_id = request.json['prode_id']
+
+		nuevo_participante = Participante(nombre, prode_id)
+
+		db.session.add(nuevo_participante)
+		db.session.commit()
+
+		return prode_schema.jsonify(nuevo_participante)
+
+@app.route('/api/participante', methods=['GET'])
+def get_participante():
+	if request.method == "GET":
+		todos = Participante.query.all()
+		r = participantes_schema.dump(todos)
+
+		return jsonify(r.data)
+
+@app.route('/api/participante/<id>', methods=['GET'])
+def get_participante_by_id(id):
+	if request.method == "GET":
+		participante = Participante.query.get(id)
+		return participante_schema.jsonify(participante)
+
+@app.route('/api/participante/<id>', methods=['PUT'])
+def actualizar_participante(id):
+	if request.method == "PUT":
+		participante = Participante.query.get(id)
+		
+		nombre = request.json['nombre']
+		prode_id = request.json['prode_id']
+
+		participante.nombre = nombre
+		participante.prode_id = prode_id
+		
+		db.session.commit()
+
+		return participante_schema.jsonify(participante)
+
+@app.route('/api/participante/<id>', methods=['DELETE'])
+def eliminar_participante(id):
+	if request.method == "DELETE":
+		participante = Participante.query.get(id)
+
+		db.session.delete(participante)
+		db.session.commit()
+
+		return participante_schema.jsonify(participante)
+
+#----------------------#
+
+#Fecha methods
+
+#----------------------#
+    
+@app.route('/api/fecha', methods=['POST'])
+def agregar_fecha():
+	if request.method == "POST":
+		numero = request.json['numero']
+		prode_id = request.json['prode_id']
+
+		nueva_fecha = Fecha(numero, prode_id)
+
+		db.session.add(nueva_fecha)
+		db.session.commit()
+
+		return fecha_schema.jsonify(nueva_fecha)
+
+@app.route('/api/fecha', methods=['GET'])
+def get_fecha():
+	if request.method == "GET":
+		todos = Fecha.query.all()
+		r = fechas_schema.dump(todos)
+
+		return jsonify(r.data)
+
+@app.route('/api/fecha/<id>', methods=['GET'])
+def get_fecha_by_id(id):
+	if request.method == "GET":
+		fecha = Fecha.query.get(id)
+		return fecha_schema.jsonify(fecha)
+
+@app.route('/api/fecha/<id>', methods=['PUT'])
+def actualizar_fecha(id):
+	if request.method == "PUT":
+		fecha = Fecha.query.get(id)
+		
+		numero = request.json['numero']
+		prode_id = request.json['prode_id']
+
+		fecha.numero = numero
+		fecha.prode_id = prode_id
+		
+		db.session.commit()
+
+		return fecha_schema.jsonify(fecha)
+
+@app.route('/api/fecha/<id>', methods=['DELETE'])
+def eliminar_fecha(id):
+	if request.method == "DELETE":
+		fecha = Fecha.query.get(id)
+
+		db.session.delete(fecha)
+		db.session.commit()
+
+		return fecha_schema.jsonify(fecha)
+
+#----------------------#
+
+#Partido methods
+
+#----------------------#
+
+@app.route('/api/partido', methods=['POST'])
+def agregar_partido():
+	if request.method == "POST":
+		fecha_id = request.json['fecha_id']
+		local = request.json['local']
+		visitante = request.json['visitante']
+
+		if(request.json['personalizado']):
+			punto_personalizado = request.json['punto_personalizado']
+			pleno_personalizado = request.json['pleno_personalizado']
+		else:
+			punto_personalizado = None
+			pleno_personalizado = None
+
+		nuevo_partido = Partido(fecha_id, local, visitante, None, punto_personalizado, pleno_personalizado)
+
+		db.session.add(nuevo_partido)
+		db.session.commit()
+
+		return partido_schema.jsonify(nuevo_partido)
+
+@app.route('/api/partido', methods=['GET'])
+def get_partido():
+	if request.method == "GET":
+		todos = Partido.query.all()
+		r = partidos_schema.dump(todos)
+
+		return jsonify(r.data)
+
+@app.route('/api/partido/<id>', methods=['GET'])
+def get_partido_by_id(id):
+	if request.method == "GET":
+		partido = Partido.query.get(id)
+		return partido_schema.jsonify(partido)
+
+@app.route('/api/partido/<id>', methods=['PUT'])
+def actualizar_partido(id):
+	if request.method == "PUT":
+		partido = Partido.query.get(id)
+		
+		fecha_id = request.json['fecha_id']
+		local = request.json['local']
+		visitante = request.json['visitante']
+
+		if(request.json['personalizado']):
+			punto_personalizado = request.json['punto_personalizado']
+			pleno_personalizado = request.json['pleno_personalizado']
+		else:
+			punto_personalizado = None
+			pleno_personalizado = None
+
+		partido.fecha_id = fecha_id
+		partido.local = local
+		partido.punto_personalizado = punto_personalizado
+		partido.pleno_personalizado = pleno_personalizado
+
+		db.session.commit()
+
+		return partido_schema.jsonify(partido)
+
+@app.route('/api/partido/<id>', methods=['DELETE'])
+def eliminar_partido(id):
+	if request.method == "DELETE":
+		partido = Partido.query.get(id)
+
+		db.session.delete(partido)
+		db.session.commit()
+
+		return partido_schema.jsonify(partido)
+
+#----------------------#
+
+#Pronostico methods
+
+#----------------------#
+
+@app.route('/api/pronostico', methods=['POST'])
+def agregar_pronostico():
+	if request.method == "POST":
+
+		participante_id = request.json['participante_id']
+		partido_id = request.json['partido_id']
+		resultado = request.json['resultado']
+
+		nuevo_pronostico = Pronostico(participante_id, partido_id, resultado)
+
+		db.session.add(nuevo_pronostico)
+		db.session.commit()
+
+		return pronostico_schema.jsonify(nuevo_pronostico)
+
+@app.route('/api/pronostico', methods=['GET'])
+def get_pronostico():
+	if request.method == "GET":
+		todos = Pronostico.query.all()
+		r = pronosticos_schema.dump(todos)
+
+		return jsonify(r.data)
+
+@app.route('/api/pronostico/<id>', methods=['GET'])
+def get_pronostico_by_id(id):
+	if request.method == "GET":
+		pronostico = Pronostico.query.get(id)
+		return pronostico_schema.jsonify(pronostico)
+
+@app.route('/api/pronostico/<id>', methods=['PUT'])
+def actualizar_pronostico(id):
+	if request.method == "PUT":
+		pronostico = Pronostico.query.get(id)
+		
+		participante_id = request.json['participante_id']
+		partido_id = request.json['partido_id']
+		resultado = request.json['resultado']
+
+		pronostico.participante_id = participante_id
+		pronostico.partido_id = partido_id
+		pronostico.resultado = resultado
+
+		db.session.commit()
+
+		return pronostico_schema.jsonify(pronostico)
+
+@app.route('/api/pronostico/<id>', methods=['DELETE'])
+def eliminar_pronostico(id):
+	if request.method == "DELETE":
+		pronostico = Pronostico.query.get(id)
+
+		db.session.delete(pronostico)
+		db.session.commit()
+
+		return pronostico_schema.jsonify(pronostico)
+
+ ######
+# html #
+ ######
+
+@app.route('/test')
+def test():
+	return render_template('test.html')
 
 @app.route('/prodes')
-def list_prodes():
-	prodes = {}
-	for p in db.session.query(Prode).all():
-		prodes[p.id] = p.nombre
+def prodes():
+	return render_template('prodes.html')
 
-	return render_template('list_prodes.html', prodes=prodes)
 
-@app.route('/prodes/<prode_id>')
-def view_prode(prode_id):
-	print(prode_id)
-	try:
-		prode_nombre = db.session.query(Prode).filter_by(id=int(prode_id)).first().nombre
-		
-		return render_template('view_prode.html', prode=prode_nombre, prode_id=prode_id)
-	except Exception as e:
-		print(e)
-		return "error"
-
-@app.route('/prodes/<prode_id>/ver_fechas')
-def ver_fechas(prode_id):
-	try:
-		fechas_obj_list = db.session.query(Fecha).filter_by(prode_id=int(prode_id)).all()
-		fechas = [x.numero for x in fechas_obj_list]
-		return render_template('ver_fechas.html', prode=prode_id, fechas=fechas)
-	except Exception as e:
-		print(e)
-		return "error"
-
-@app.route('/prodes/<prode_id>/ver_fechas/<fecha_numero>')
-def ver_fecha_numero(prode_id, fecha_numero):
-	try:
-		prode_nombre = db.session.query(Prode).filter_by(id=int(prode_id)).first().nombre
-		
-		fecha_id = db.session.query(Fecha).filter_by(prode_id=int(prode_id), numero=fecha_numero).first().id
-
-		partidos_obj_list = db.session.query(Partido).filter_by(fecha_id=int(fecha_id)).all()
-		partidos = [(x.local, x.visitante) for x in partidos_obj_list]
-		
-		return render_template('ver_fecha_numero.html', prode=prode_id, prode_nombre=prode_nombre, partidos=partidos, fecha_numero=fecha_numero)
-	except Exception as e:
-		print(e)
-		return "error"
-
-	
-@app.route('/prodes/<prode_id>/agregar_fecha', methods=['GET', 'POST'])
-def agregar_fecha(prode_id):
-	errors = []
-	prode_nombre = db.session.query(Prode).filter_by(id=int(prode_id)).first().nombre
-		
-	if request.method == "POST": 
-		try:
-			cantidad_equipos = request.form['cantidad']
-			cantidad_equipos = (int(cantidad_equipos)*2)
-			numero = request.form['numero']
-			equipos = []
-			for i in range(1, cantidad_equipos+1):
-				equipos.append(request.form['equipo' + str(i)])
-		except Exception as e:
-			print(e)
-			errors.append("unable to ")
-			return render_template('agregar_fecha.html', errors=errors)
-
-		try:
-			id_new_f = db.session.query(Fecha).order_by(Fecha.id.desc()).first().id + 1
-
-			print(id_new_f)
-				
-			partidos = []
-			#fecha_id, local, visitante
-			for i in range(0, len(equipos), 2):
-				partidos.append(Partido(id_new_f, equipos[i], equipos[i+1]))
-			
-			db.session.add_all(partidos)
-				
-
-			for p in partidos:
-				db.session.add(p)
-
-			f = Fecha(numero, int(prode_id), partidos)
-			db.session.add(f)
-
-			db.session.commit()
-
-		except Exception as e:
-			print(e)
-			errors.append("No se pudo agregar fecha a bd")
-			
-
-	return render_template('agregar_fecha.html', errors=errors, prode_nombre=prode_nombre, prode_id=prode_id)
-
-@app.route('/prodes/<prode_id>/ver_participantes')
-def ver_participantes(prode_id):
-	try:
-		puntos_obj_list = db.session.query(Punto).filter_by(prode_id=int(prode_id)).all()
-
-		participantes_obj_list = []
-		for p in puntos_obj_list:
-			participantes_obj_list.append(db.session.query(Participante).filter_by(id=p.participante_id).first())
-		
-
-		participantes = {x.id: x.nombre for x in participantes_obj_list}
-		return render_template('ver_participantes.html', prode=prode_id, participantes=participantes)
-	except Exception as e:
-		print(e)
-		return "error"
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
--- models.py
+++ models.py
@@ -1,21 +1,5 @@
 from app import db
-
-
-'''class Result(db.Model):
-    __tablename__ = 'results'
-
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
-
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
-
-    def __repr__(self):
-        return '<id {}>'.format(self.id)'''
+from flask_marshmallow import Marshmallow
 
 class Prode(db.Model):
     __tablename__ = 'prodes'
@@ -23,42 +7,28 @@ class Prode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String())
     fechas = db.relationship("Fecha", backref='prodes')
+    participantes =  db.relationship("Participante", backref='prodes')
 
     def __init__(self, nombre):
         self.nombre = nombre
-
 
 class Participante(db.Model):
     __tablename__ = 'participantes'
 
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String())
-    puntos = db.relationship("Punto", backref='participantes')
     pronosticos = db.relationship("Pronostico", backref='participantes')
+    prode_id = db.Column(db.Integer, db.ForeignKey('prodes.id'),
+        nullable=False)
+
+    puntos = db.Column(db.Integer)
+    plenos = db.Column(db.Integer)
 
     def __init__(self, nombre, prode_id):
         self.nombre = nombre
-        self.puntos = [Punto(prode_id, id)]
-
-
-class Punto(db.Model):
-    __tablename__ = 'puntos'
-
-    id = db.Column(db.Integer, primary_key=True)
-    cantidad = db.Column(db.Integer)
-    plenos = db.Column(db.Integer)
-
-    prode_id = db.Column(db.Integer, db.ForeignKey('prodes.id'),
-        nullable=False)
-    participante_id = db.Column(db.Integer, db.ForeignKey('participantes.id'),
-        nullable=False)
-
-    def __init__(self, prode_id, participante_id):
-        self.cantidad = 0
-        self.plenos = 0
-
         self.prode_id = prode_id
-        self.participante_id = participante_id
+        self.puntos = 0
+        self.plenos = 0
 
 class Partido(db.Model):
     __tablename__ = 'partidos'
@@ -97,11 +67,10 @@ class Fecha(db.Model):
 
     partidos = db.relationship("Partido", backref='fechas')
 
-    def __init__(self, numero, prode_id, partidos):
+    def __init__(self, numero, prode_id):
         self.numero = numero
         self.prode_id = prode_id
 
-        self.partidos = partidos
 
 class Pronostico(db.Model):
     __tablename__ = 'pronosticos'
@@ -114,3 +83,10 @@ class Pronostico(db.Model):
         nullable=False)
 
     resultado = db.Column(db.String())
+
+    def __init__(self, participante_id, partido_id, resultado):
+        self.participante_id = participante_id
+        self.partido_id = partido_id
+        self.resultado = resultado 
+
+
